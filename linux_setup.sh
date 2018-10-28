fe0208a3511535a0e4b0f42a755310005b7ad47e
John <34991353+jkaramichos@users.noreply.github.com>
2018-10-28 11:30:30 -0400

    Add files via upload

---
+++ linux_setup.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+
+sudo apt install git
+sudo apt install vim
+git clone https://github.com/jkaramichos/my-vimrc
+
+
+cd my-vimrc
+chmod +x install.sh
+./install.sh
+
+printf "function cd {" >> ~/.bashrc
+printf "\n	builtin cd "$@" && ls -F" >> ~/.bashrc
+printf "\n }" >> ~/.bashrc
+
+printf '\nbind ' >> ~/.bashrc && printf "'" >> ~/.bashrc && echo -n "\e[A" >> ~/.bashrc && printf ": history-search-backward'" >> ~/.bashrc
+printf '\nbind ' >> ~/.bashrc && printf "'" >> ~/.bashrc && echo -n "\e[B" >> ~/.bashrc && printf ": history-search-forward'" >> ~/.bashrc
+
+printf '\nexport PATH=$PATH:/snap/bin' >> ~/.bashrc
+printf "\nexport PROMPT_COMMAND" >> ~/.bashrc
+echo '="echo -n \[\$(date +%F-%T)\]\ "' >> ~/.bashrc
