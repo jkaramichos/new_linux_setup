72bb9366bc06f555f3cdd3f2d390d3929fb44dc1
John <34991353+jkaramichos@users.noreply.github.com>
2023-03-12 17:46:52 -0400

    Update linux_setup.sh

--- linux_setup.sh
+++ linux_setup.sh
@@ -10,9 +10,9 @@ cd my-vimrc
 chmod +x install.sh
 ./install.sh
 
-printf "function cd {" >> ~/.bashrc
-printf "\n	builtin cd "$@" && ls -F" >> ~/.bashrc
-printf "\n }" >> ~/.bashrc
+#printf "function cd {" >> ~/.bashrc
+#printf "\n	builtin cd "$@" && ls -F" >> ~/.bashrc
+#printf "\n }" >> ~/.bashrc
 
 printf '\nbind ' >> ~/.bashrc && printf "'" >> ~/.bashrc && echo -n "\e[A" >> ~/.bashrc && printf ": history-search-backward'" >> ~/.bashrc
 printf '\nbind ' >> ~/.bashrc && printf "'" >> ~/.bashrc && echo -n "\e[B" >> ~/.bashrc && printf ": history-search-forward'" >> ~/.bashrc
